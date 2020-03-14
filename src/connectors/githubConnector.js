86990d1611c2b4fad550c02777c98cf76dc4508e
Lacour Vincent <lacour.vincent.app@gmail.com>
2020-03-14 19:19:29 +0100

    Upgrade dependencies

--- src/connectors/githubConnector.js
+++ src/connectors/githubConnector.js
@@ -1,23 +1,15 @@
 const GITHUB_API_URL = "https://api.github.com";
 
 export const fetchUser = async username => {
-  try {
-    const response = await fetch(`${GITHUB_API_URL}/users/${username}`);
-    if (!response.ok) throw new Error(response.statusText);
-    const user = await response.json();
-    return user;
-  } catch (err) {
-    throw err;
-  }
+  const response = await fetch(`${GITHUB_API_URL}/users/${username}`);
+  if (!response.ok) throw new Error(response.statusText);
+  const user = await response.json();
+  return user;
 };
 
 export const fetchUserRepositories = async username => {
-  try {
-    const response = await fetch(`${GITHUB_API_URL}/users/${username}/repos`);
-    if (!response.ok) throw new Error(response.statusText);
-    const userRepositories = await response.json();
-    return userRepositories;
-  } catch (err) {
-    throw err;
-  }
+  const response = await fetch(`${GITHUB_API_URL}/users/${username}/repos`);
+  if (!response.ok) throw new Error(response.statusText);
+  const userRepositories = await response.json();
+  return userRepositories;
 };
